a67617382f9c8133f4ab7f2829bc9a0d1397e028
Həsən İsmayılov <hesenismayilov44@gmail.com>
2020-12-28 23:12:01 +0400

    changed

--- Progustad/Progustad/settings.py
+++ Progustad/Progustad/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1_g*n2)b=jfpetq&#^1#8s6l+e+q8f-%*$lb!yiju&j4srkewq'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
